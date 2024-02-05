9af50a795906e3e13a4efcea4e6090bd5dae629e
Khemz-Developer <jalithapramod@gmail.com>
2024-02-05 23:11:11 +0530

    Banners and Category Section Is Finished

--- Food_Ordering_Client/src/pages/home/home.jsx
+++ Food_Ordering_Client/src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../components/Banner'
+import Categories from './Categories'
 
 const home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Categories></Categories>
     </div>
   )
 }
