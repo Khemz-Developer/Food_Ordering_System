eaaa4630b77c00d6b57f8cafd70d16a18dce86ca
Khemz-Developer <jalithapramod@gmail.com>
2024-02-08 00:31:02 +0530

    Review Section Finished in HomePage

--- Food_Ordering_Client/src/pages/home/home.jsx
+++ Food_Ordering_Client/src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../../components/Banner'
 import Categories from './Categories'
 import SpecialDishes from './SpecialDishes'
+import Testimonials from './Testimonials'
 
 const home = () => {
   return (
@@ -9,6 +10,7 @@ const home = () => {
       <Banner></Banner>
       <Categories></Categories>
       <SpecialDishes/>
+      <Testimonials/>
     </div>
   )
 }
