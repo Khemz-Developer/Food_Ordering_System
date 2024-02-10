1aa6026b35221065687963d7ebe1f5f7a597a544
Khemz-Developer <jalithapramod@gmail.com>
2024-02-10 19:35:04 +0530

    Items fetch in menu side finished and filtering and shorting option is finished.

--- Food_Ordering_Client/src/pages/shop/Menu.jsx
+++ Food_Ordering_Client/src/pages/shop/Menu.jsx
@@ -1,34 +1,79 @@
 import React, { useEffect, useState } from "react";
-
+import Cards from "../../components/Cards";
+import { FaFilter } from "react-icons/fa";
 const Menu = () => {
-    
-  const[menu,setMenu] = useState([])
-  const[filteredItems,setFilteredItems] = useState([]);
-  const[selectedCategory,setSelectedCategory] = useState("all");
-  const[sortOption, setsortOption] = useState("default")
+  const [menu, setMenu] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
 
   //loading data
-  useEffect(()=>{
+  useEffect(() => {
     //fetch data from the backend --
-    const fetchData = async()=>{
-        try{
-            const responce = await fetch("/menu.json");
-            const data = responce.json();
-           // console.log(data);
-            setMenu(data);
-        } catch(error){
-            console.log("Error fetching data ",error);
-        }
+    const fetchData = async () => {
+      try {
+        const responce = await fetch("/menu.json");
+        const data = await responce.json();
         
-      };
+        setMenu(data);
+        setFilteredItems(data);
+      } catch (error) {
+        console.log("Error fetching data ", error);
+      }
+    };
 
-      //Call the function
-      fetchData()
-  },[]) 
-    
+    //Call the function
+    fetchData();
+  }, []);
+
+  // Filtering Data Based on Category
+
+  const filterItems = (category) => {
+    const filtered =
+      category === "all"
+        ? menu
+        : menu.filter((item) => item.category === category);
+
+    setFilteredItems(filtered);
+    setSelectedCategory(category);
+  };
+
+  //show all data
+  const showAll = () => {
+    setFilteredItems(menu);
+    setSelectedCategory("all");
+  };
+
+  const handleSortChange = (option) => {
+    setSortOption(option);
+
+    let sortedItems = [...filteredItems];
+
+    //logic
+    switch (option) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      case "high-to-low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+
+    setFilteredItems(sortedItems)
+  };
+
+  // shorting based on A-Z ,Z-A ,Low - High Pricing
   return (
     <div>
-      {/* menu  */}
+      {/* menu banner */}
       <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
         <div className="flex flex-col items-center justify-center gap-8 py-48 ">
           {/*test*/}
@@ -52,7 +97,41 @@ const Menu = () => {
       {/* menu shop section */}
 
       <div className="section-container">
-        
+        {/* filtered and sorting*/}
+        <div className="flex flex-col flex-wrap items-center md:justify-between md:flex-row">
+        {/* all category buttons */}
+        <div className="flex flex-row flex-wrap justify-start gap-4 mt-5 md:items-center">
+          <button className={selectedCategory === "all" ? "active" : ""} onClick={showAll}>All</button>
+          <button className={selectedCategory === "salad" ? "active" : ""}  onClick={()=> filterItems("salad")}>Salad</button>
+          <button className={selectedCategory === "pizza" ? "active" : ""}  onClick={()=> filterItems("pizza")}>Pizza</button>
+          <button className={selectedCategory === "soup" ? "active" : ""}  onClick={()=> filterItems("soup")}>Soup</button>
+          <button className={selectedCategory === "dessert" ? "active" : ""}  onClick={()=> filterItems("dessert")}>Dessert</button>
+          <button className={selectedCategory === "drinks" ? "active" : ""}  onClick={()=> filterItems("drinks")}>Drinks</button>
+        </div>
+        {/* sorting filter */}
+
+        <div className="flex justify-end mt-4 rounded-sm">
+          <div className="p-2 bg-black">
+            <FaFilter className="w-4 h-4 text-white"/>
+          </div>
+          
+          {/* sorting options */}
+          <select name="sort" id="sort" onChange={(e)=>handleSortChange(e.target.value)} value={sortOption} className="px-2 py-1 text-white bg-black rounded-sm">
+            <option value="default">Default</option>
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+            <option value="low-to-high">Low-to-High</option>
+            <option value="high-to-low">High-to-Low</option>
+          </select>
+        </div>
+        </div>
+
+        {/* product cards*/}
+        <div className="grid grid-cols-1 gap-2 mt-3 md:grid-cols-4 sm:grid-col-2">  
+            {filteredItems.map((item, i) => (
+              <Cards key={i} item={item}></Cards>
+            ))}
+        </div>
       </div>
     </div>
   );
